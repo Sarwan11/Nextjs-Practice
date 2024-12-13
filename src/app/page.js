3630c6865528027c45a5c971040e9b21517bf205
Sarwan11 <thalasarwan45@gmail.com>
2024-12-13 11:52:27 +0530

    do some changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,57 @@
-"use client";
-import React, { useState } from "react";
+// "use client";
+// import React, { useState } from "react";
+
+// const page = () => {
+//   const [to, setTo] = useState("Alice");
+//   const [message, setMessage] = useState("hello");
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     setTimeout(() => {
+//       alert(`you say the ${message} to ${to}`);
+//     }, 2000);
+//   };
+//   return (
+//     <div>
+//       <form action="" onSubmit={handleSubmit}>
+//         <label>To:</label>
+//         <select name="" id="">
+//           <option value="">Alice</option>
+//           <option value="">Nobita</option>
+//           <option value="">Vardaan</option>
+//         </select>
+//         <textarea
+//           defaultValue={message}
+//           onChange={(e) => setMessage(e.target.value)}
+//         />
+
+//         <button type="submit">submit</button>
+//       </form>
+//     </div>
+//   );
+// };
+import React from "react";
 
 const page = () => {
-  const [to, setTo] = useState("Alice");
-  const [message, setMessage] = useState("hello");
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setTimeout(() => {
-      alert(`you say the ${message} to ${to}`);
-    }, 2000);
-  };
+  let skills = [
+    "HTML",
+    "CSS",
+    "Javascript",
+    "TailwindCSS",
+    "React",
+    "Redux",
+    "NextJs",
+  ];
   return (
     <div>
-      <form action="" onSubmit={handleSubmit}>
-        <label>To:</label>
-        <select name="" id="">
-          <option value="">Alice</option>
-          <option value="">Nobita</option>
-          <option value="">Vardaan</option>
-        </select>
-        <textarea
-          defaultValue={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
+      {
+        skills.forEach((val) => {
+          return val;
+        })
 
-        <button type="submit">submit</button>
-      </form>
+        // console.log(`Skills: ${mySkills}`);
+      }
     </div>
   );
 };
+
 export default page;
